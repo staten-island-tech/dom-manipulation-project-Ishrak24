440addf6a2226cc7d9dffe46abc49d30d558ed4c
Ishrak24 <Ishrakmiah04@gmail.com>
2022-11-07 13:16:18 -0500

    testing

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const DOMSelectors = {
+/*const DOMSelectors = {
   button: document.getElementById("btn"),
   text: document.querySelector("#text"),
   box: document.getElementById("big-black-box"),
   points: document.querySelectorAll(".point"),
 };
 
-function backgoundAndText(background, text) {
+function backgoundAndText(backgound, text) {
   backgound.style.backgroundColor = "red";
   text.textContent = "This is now a bigger red box";
   text.style.fontSize = "40px";
@@ -14,3 +14,17 @@ function backgoundAndText(background, text) {
 DOMSelectors.button.addEventListener("click", function () {
   backgoundAndText(DOMSelectors.box, DOMSelectors.text);
 });
+
+DOMSelectors.button.addEventListener("click", function () {
+  backgroundAndText(DOMSelectors.box, DOMSelectors.text);
+});
+
+function changeLi() {
+  let pointIndex = 2;
+  DOMSelectors.points.forEach((point) => {
+    point.addEventListener("click", function () {
+      point.textContent = `Hello I am point ${pointIndex}`;
+      pointIndex++;
+    });
+  });
+}*/
